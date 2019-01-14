0592e970becf367caf73653837c90c12f5b6f05a
Ryan-Milton <mr.ryan.milton@gmail.com>
2019-01-13 17:55:55 -0800

    sends email

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ var express = require('express'),
     path = require('path'),
     nodeMailer = require('nodemailer'),
     bodyParser = require('body-parser');
+    require('dotenv').config();
 
     var app = express();
     app.set('view engine', 'ejs');
@@ -13,31 +14,33 @@ var express = require('express'),
       res.render('index');
     });
     app.post('/send-email', function (req, res) {
-      let transporter = nodeMailer.createTransport({
-          host: 'smtp.gmail.com',
-          port: 465,
-          secure: true,
-          auth: {
-              user: 'xxx@xx.com',
-              pass: 'xxxx'
-          }
-      });
-      let mailOptions = {
-          from: '"Krunal Lathiya" <xx@gmail.com>', // sender address
-          to: req.body.to, // list of receivers
-          subject: req.body.subject, // Subject line
-          text: req.body.body, // plain text body
-          html: '<b>NodeJS Email Tutorial</b>' // html body
-      };
+        console.log('request body subject ++++++++', req.body.subject);
+        console.log('request body body ++++++++', req.body.body);
 
-      transporter.sendMail(mailOptions, (error, info) => {
-          if (error) {
-              return console.log(error);
-          }
-          console.log('Message %s sent: %s', info.messageId, info.response);
-              res.render('index');
-          });
-      });
-          app.listen(port, function(){
-            console.log('Server is running at port: ',port);
-          });
+        let transporter = nodeMailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true,
+            auth: {
+                user: process.env.SENDER,
+                pass: process.env.SENDER_PASSWORD
+            }
+        });
+        let mailOptions = {
+            from: ' "Newsletter Request" ', // sender address
+            to: process.env.RECIEVER, // list of receivers
+            subject: req.body.subject, // Subject line
+            html: req.body.body // email body
+        };
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message %s sent: %s', info.messageId, info.response);
+                res.render('index');
+            });
+        });
+            app.listen(port, function(){
+              console.log('Server is running at port: ',port);
+            });
